9f09a63e7edbd3c0bf30d7f86699d7a791e26273
Mukesh Yadav <mukeshyadav@gmail.com>
2020-07-11 18:48:51 +0530

    initial commit: setting the project structure

---
+++ api/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const authRouter = require("./src/routes/auth");
+const storiesRouter = require("./src/routes/stories");
+const authenticate = require("./src/auth_helper").authenticate;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use(authRouter);
+router.use("/stories", authenticate, storiesRouter);
+app.use("/api/v1", router);
+
+let port = 3000;
+app.listen(port);
+console.log(`Api server listerning at port ${port}`);
+console.log(
+  `Swager API Doc is available now at http://localhost:${port}/api-docs`
+);
